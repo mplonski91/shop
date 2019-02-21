4ae687205588fbee3d3a7ec4a2a2d22dff2287c9
Maciek <maciej.plonski@pagepro.pl>
2019-02-21 17:15:48 +0100

    Updates

---
+++ precache-manifest.0bc697da58892ea717d3c67bec028d46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a4c97e913adeac98b80",
+    "url": "/shop/static/js/runtime~main.3a4c97e9.js"
+  },
+  {
+    "revision": "56f6522480005d6e9b9f",
+    "url": "/shop/static/js/main.56f65224.chunk.js"
+  },
+  {
+    "revision": "33d37b6f9034fb813c5c",
+    "url": "/shop/static/js/1.33d37b6f.chunk.js"
+  },
+  {
+    "revision": "56f6522480005d6e9b9f",
+    "url": "/shop/static/css/main.d28f7954.chunk.css"
+  },
+  {
+    "revision": "8e50e580289b11c4fd47c20668da4fa9",
+    "url": "/shop/index.html"
+  }
+];
